3bf4bf106880d3ef990e494d1fb1174d80e77a0f
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-03 17:52:13 -0300

    Listas dos estressados

---
+++ ATV02/Buscar/Contagem/calcular_stress_medio.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+using namespace std;
+
+float calcular_stress_medio( int vet[], int tam) {
+    float soma = 0; 
+    int i;
+    
+    for ( i = 0; i < tam; i++ ) {
+        soma += vet[ i ];
+    }
+    return soma / tam;
+}
+
+int main()
+{
+    int vetX[ 5 ], i;
+    float media; 
+
+    for ( i = 0; i < 5; i++ ) {
+    cout << "Digite um valor: " << endl; 
+    cin >> vetX[ i ];
+    }
+    
+    media = calcular_stress_medio(vetX, 5);
+    cout << "A media de stress: " << media << endl; 
+
+    return 0;
+}
