62fd4bf7077ce1e6da66b0b0dfb0ea3cef42ba0e
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-04 13:25:17 -0300

    Listas dos estressados

--- ATV02/Buscar/Existe.cpp
+++ ATV02/Buscar/Existe.cpp
@@ -1,54 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int set_existe( int x[], int tam, int num) {
-    int ex = 0;
-    int i;
+int existe ( int vet[], int tam) {
 
-    for (i = 0; i < num; i++) {
-        if (x[i] == num) {
+int i, num, ex = 0;
+
+    cout << "Digite um valor que voce deseja verificar se existe na fila: " << endl;
+    cin >> num;
+
+    for (i = 0; i < tam; i++) {
+        if ( num == vet[i] ) {
             ex = 1;
         }
     }
-
-return ex;
+            if( ex == 1){
+                cout << "Numero digitado existe na fila " << endl;
+            }
+            else {
+                cout << "Numero digitado nao existe " << endl;
+            }
 } 
 
-void set_vetX( int vetor[], int tam, int num) {
-
-    int i;
-    for (i = 0; i < tam; i++) {
-        cout << "Digite o elemento do vetor " << i << ":" << endl;
-        cin >> vetor[i];
-    }
-} 
-
-int main()
+int main ()
 {
 
-    int tam = 0;
-    cout << "Digite o tamando da fila: \n";
-    cin >> tam; 
+    int vetX[5], i;
 
-    int stress[tam];
-    int prest = 0;
-    int i; 
+        for (i = 0; i < 5; i++) {
+			    cout << "Digite um valor: " << endl;
+			    cin >> vetX[ i ];
+	    }
+        
+        existe ( vetX, 5);
 
-set_vetX(stress, tam, prest);
-
-    for ( i = 0; i < tam; i++) {
-        int num = 0;
-        cout << "Digite o valor que voce deseja verificar se existe na fila: ";
-        cin >> num; 
-
-        prest = set_existe( stress, tam, num);
-            if (prest == 1) {
-                cout << "Valor existente na fila \n" << endl; 
-            } 
-            else {
-                cout << "Valor nao existente na fila" << endl;
-            }
-            
-            break;
-    }
+    return 0;
 }
