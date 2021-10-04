a1dc00fd7112292166f6f680e1839cae2b3bf609
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-04 10:26:34 -0300

    Listas dos estressados

--- ATV02/Buscar/Melhor caso/procurar_menor.cpp
+++ ATV02/Buscar/Melhor caso/procurar_menor.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int procurar_menor ( int vet[], int tam) {
+
+int i, menor = vet[ 0 ];
+
+    for (i = 0; i < 5; i++){
+              if ( vet[ i ] < menor ){
+                  menor = vet[ i ];
+              }
+          }
+
+    cout << "Menor valor encontrado: " << menor << endl;
+}
+
 int main()
 {
-    int vetX[5];
-    int menor = vetX[ 0 ], i;
+    int vetX[5], i;
 
     for (i = 0; i < 5; i++){
         cout << "Digite um valor: " << endl;
         cin >> vetX[i]; 
     }
-          
-          for (i = 0; i < 5; i++){
-              if ( vetX[i] < menor ){
-                  menor = vetX[i];
-              }
-          }
-    
-    cout << "Menor valor encontrado: " << menor << endl;
+
+    procurar_menor( vetX, 5 );          
 
 return 0;
 }
