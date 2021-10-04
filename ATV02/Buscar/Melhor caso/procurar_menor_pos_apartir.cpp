830d3dcbc2080fdd1aa9565d036b63c3cd629ffa
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-04 10:22:35 -0300

    Listas dos estressados

--- ATV02/Buscar/Melhor caso/procurar_menor_pos_apartir.cpp
+++ ATV02/Buscar/Melhor caso/procurar_menor_pos_apartir.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int procurar_menor_pos_apartir ( int vet[], int tam) {
+
+int i, num, PMPA;
+int encontrado = 0, contador = 0;
+    
+    cout << "Digite um numero que voce deseja procurar: " << endl;
+    cin >> num;
+
+        for (i = 0; i < tam; i++) {
+            if ( vet[ i ] == num) {
+                encontrado = 1;
+                PMPA = i;
+                contador++;
+            }
+        }        
+                if (encontrado == 0 ) {
+                    cout << "Numero nao existe: " << endl;
+                }
+                else {
+                    cout << "Numero " << num << " aparece " << contador << " x no vetor." << " E sua proxima posicao: " << PMPA << endl;
+                }
+}
+
 int main()
 {
 
@@ -13,24 +36,7 @@ int i, num, procurar_menor_pos_apos;
         cin >> vetX[i];
     }
 
-
-    cout << "Digite um numero que voce deseja procurar: " << endl;
-    cin >> num;
-
-        for (i = 0; i < 5; i++) {
-            if ( vetX[i] == num) {
-                encontrado = 1;
-                procurar_menor_pos_apos = i;
-                contador++;
-            }
-        }
-        
-                if (encontrado == 0 ) {
-                    cout << "Numero nao existe: " << endl;
-                }
-                else {
-                    cout << "Numero " << num << " aparece " << contador << " x no vetor." << " E sua proxima posicao: " << procurar_menor_pos_apos << endl;
-                }
+    procurar_menor_pos_apartir( vetX, 5);
 
     return 0;
 }
