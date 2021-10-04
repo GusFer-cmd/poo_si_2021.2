07fbdc7832f8774c15b14c0177b527e5564b10cb
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-04 11:28:12 -0300

    Listas dos estressados

--- ATV02/Buscar/procurar_valor_apartir.cpp
+++ ATV02/Buscar/procurar_valor_apartir.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int procurar_valor_apartir( int vet[], int tam) {
 
-    int vetX[5];
-	int  i, num, procurar_valor_apartir;
-    int encontrado = 0, contador = 0;
-	
-		for (int i = 0; i < 5; i++) {
-			cout << "Digite um valor: " << endl;
-			cin >> vetX[i];
-		}
-	
-	cout << "Digite um numero que voce deseja procurar: " << endl;
+int i, num;
+int Pos = 0, encontrado = 0, contador = 0;
+
+    cout << "Digite um numero que voce deseja procurar: " << endl;
 	cin >> num;
 	
-			for (int i = 0; i < 5; i++) {
-				if (vetX[ i ] == num) {
+			for (int i = 0; i < tam; i++) {
+				if (vet[ i ] == num) {
 					encontrado = 1;
-					procurar_valor_apartir = i;
+					Pos = i;
             		contador++;
         	}
 				}
@@ -28,8 +21,24 @@ int main()
 		        	cout << "Numero nao existente" << endl;
 	        	}
 	        	else {
-		        	cout << "Numero " << num << " aparece " << contador << "x no vetor." << " E sua proxima posicao: " << procurar_valor_apartir << endl;
+		        	cout << "Numero " << num << " aparece " << contador << "x no vetor." << " E sua proxima posicao: " << Pos << endl;
 	       		}
+
+}
+
+int main()
+{
+
+int vetX[5];
+int  i;
+ 
+	
+		for (int i = 0; i < 5; i++) {
+			cout << "Digite um valor: " << endl;
+			cin >> vetX[i];
+		}
+	
+	procurar_valor_apartir( vetX, 5 );
 
 return 0;
 }
