837d098d7615a9a218624444fcd3ab35ed6cf013
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-07 11:40:13 -0300

    Listas dos estressados

---
+++ ATV02/Acesso/sortear.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int sortear(int vet[], int tam) {
+
+int i; 
+
+    for (i = 0; i < tam; i++) {
+        srand(vet[i]);
+        cout << "Sorteio: " << rand()%tam << endl;
+    }
+}
+
+int main() 
+{
+
+int vetX[ 6 ], i;
+
+    for (i = 0; i < 6; i++) {
+        cout << "Digite um valor: " << endl;
+        cin >> vetX[i];
+    }
+
+    sortear(vetX, 6);
+
+return 0;
+}
