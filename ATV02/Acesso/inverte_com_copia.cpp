10a0b98534c7c522bd404bd18c22108b851aa8e0
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-07 10:58:15 -0300

    Listas dos estressados

---
+++ ATV02/Acesso/inverte_com_copia.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int inverter_com_copia( int vet[], int tam) {
+
+int i, j, vetI[ i ];
+
+        for (i = 0, j = 5; i < 6 && j >= 0; i++, j--) {
+            vetI[ i ] = vet[ j ];
+            cout << "O vetor invertido: " << vetI[ i ] << endl;
+        }
+} 
+
+int main()
+{
+
+int vetX[ 6 ], vetI[ 6 ], i, j;
+
+    for (i = 0; i < 6; i++) {
+        cout << "Digite um valor: " << endl;
+        cin >> vetX[ i ];
+    }
+
+    inverter_com_copia( vetX, 6);
+
+return 0;
+}
