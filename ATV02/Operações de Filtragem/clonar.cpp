a8ad636f3d2629fb0cc2bc2ca4b9b4f2a4d655e1
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-07 13:45:15 -0300

    Listas dos estressados

---
+++ ATV02/Operações de Filtragem/clonar.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int clonar(int vet[], int tam, int vetClone[]) {
+
+int i;
+
+    cout << "O vetor clone: ";
+
+    for (i = 0; i < tam; i++) {
+        vetClone[ i ] = vet[ i ];
+        cout << vetClone[ i ] << "  ";
+    }
+}
+
+int main()
+{
+
+int vetX[ 6 ], vetClonado[ 6 ], i;
+
+    for (i = 0; i < 6; i++) {
+        cout << "Digite um valor: " << endl;
+        cin >> vetX[ i ];
+    }   
+
+    clonar(vetX, 6, vetClonado);
+
+return 0;
+}
