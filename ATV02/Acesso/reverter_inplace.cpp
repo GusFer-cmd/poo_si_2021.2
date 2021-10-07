5551cc1888a63243119511af5c2f6fb21d9a486e
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-07 11:12:19 -0300

    Listas dos estressados

---
+++ ATV02/Acesso/reverter_inplace.cpp
@@ -0,0 +1,34 @@
+# include <iostream>
+using namespace std;
+
+int reverter_inplace( int vet[], int tam ) {
+
+int j = tam-1, i, aux;
+
+    for ( i = 0; i < tam/2; i++ ) {
+        aux = vet[ i ];
+        vet[ i ] = vet[ j ];
+        vet[ j ] = aux;
+        j--;
+    }
+
+        for ( i = 0; i < tam; i++) {
+            cout << "Vetor invertido: " << vet[ i ] << endl;
+        }
+
+} 
+
+int main()
+{
+
+int vetX[ 6 ], i;
+
+    for ( i = 0; i < 6; i++) {
+        cout << "Digite um valor: " << endl;
+        cin >> vetX[ i ];    
+    }
+
+    reverter_inplace( vetX, 6);
+
+return 0;
+}
