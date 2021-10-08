10836c57a1ca8c909087f3c7a534428e42fea7d8
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-08 11:08:01 -0300

    Listas dos estressados

---
+++ ATV02/Bloco 2/Conjunto/abandonados.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int abandonados( int vet[], int tam) {
+
+int i, j;
+
+    bool exist;
+    for (i = 0; i < tam; i++) {
+        exist = false;
+        for (j = 0; j < i; j++) {
+            if(vet[i] == vet[j]){
+                exist = true;
+                break;
+            }
+                if(vet[i] < 0){ //Laço para verificar os números negativos
+                    exist = true;
+                    break;
+                }     
+        }    
+        if(exist == true) {
+            cout << vet[ i ] << endl;
+        }
+    }
+}
+
+int main()
+{
+
+int vetX[ 6 ], i;
+
+    for( i = 0; i < 6; i++) {
+        cout << "Digite um valor: ";
+        cin >> vetX[i];
+    }
+
+    abandonados(vetX, 6);
+
+return 0;
+}
