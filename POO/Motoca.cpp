a561133312e00d6d8f113631c0b8dc5a1117f35e
Gustavo Fernandes <g.gustavo5556@gmail.com>
2021-10-22 14:36:11 -0300

    POO/Arcade

--- POO/Motoca.cpp
+++ POO/Motoca.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 struct Pessoa {
@@ -15,6 +16,10 @@ struct Pessoa {
     }
 }; 
 
+void mostrar( Pessoa& pessoa ) {
+        cout << pessoa.nome << " tem " << pessoa.idade << " anos " << endl; 
+    }
+
 struct Moto {
     int power{1};
     int tempo{0};
@@ -23,9 +28,14 @@ struct Moto {
         power{power}, tempo{tempo}, pessoa{pessoa} {
     }      
 
+    friend ostream& operator<<(ostream& os, const Moto& moto) {
+        os << "Potencia: " << moto.power << " \n";
+        os << "Tempo: " << moto.tempo << " anos\n";    
+    }
+
     bool inserirPessoa(Pessoa* pessoa) {
         if(this->pessoa != nullptr) {
-            cout << "Ja tem uma crianca\n";
+            cout << "Ja tem uma crianca na motoca.\n";
             return false;
         }
         this->pessoa = pessoa;
@@ -34,39 +44,52 @@ struct Moto {
 
     Pessoa* removerPessoa() {
         if(this->pessoa == nullptr) {
-            cout << "Nao tem crianca\n";
+            cout << "Nao tem crianca.\n";
             return nullptr;
         }
-        Pessoa* pessoa = this->pessoa;
-        this->pessoa = nullptr;
-        return pessoa; 
+        return exchange(this->pessoa, nullptr);
     }
 
     void buy(int tempo) {
         if(this->tempo == 0) {
-            cout << "Tempo acabou. Deseja comprar mais tempo?!\n";
-        }
+            cout << "Tempo acabou. Compre mais tempo!\n";
+        } else {
+            this->tempo += tempo;
+            cout << "Tempo adicionado com sucesso!\n";
+        }   
+        
+        cout << this->tempo << " minutos\n"; 
     }
 
     void drive(int tempo) {
         if(this->tempo > 0) {
-            cout << "O tempo ainda não acabou, continue a dirigir\n";
+            cout << "O tempo ainda nao acabou, continue a dirigir!\n";
         }
     }
 
     void honk(){
-        cout << "Pem\n";
+        if (this->power == 0) {
+            cout << "Pem\n";
+        } else {
+            cout << "Peem\n";
+        }
     }
 };
 
 int main() {
     Pessoa pessoa ("Gustavo", 7);
-    
-    Moto moto(3, 5, &pessoa );
-    moto.pessoa->idade -= 1;
-    
-    cout << pessoa.idade << "\n";
-    cout << moto.pessoa->idade << "\n";
+    mostrar(pessoa);
+
+    Pessoa pessoa2 ("Lucas", 8);
+
+    Moto moto(3, 5, &pessoa);
+    cout << "A potencia: " << moto.power << endl;
+    cout << "O tempo: " << moto.tempo << endl;
+
+    moto.drive(4);
+    moto.honk();
+    moto.buy(1);
+    moto.inserirPessoa(&pessoa2);
 
 return 0;
 }
