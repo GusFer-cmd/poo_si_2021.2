860d895978b9fedb7605614b53b75f69f4389c12
Gustavo Fernandes <g.gustavo5556@gmail.com>
2022-02-01 09:39:34 -0300

    Listas dos estressados

--- ATV02/Bloco 2/Acesso/embaralhar.cpp
+++ ATV02/Bloco 2/Acesso/embaralhar.cpp
@@ -1,32 +1,28 @@
 #include <iostream> 
 #include <cstdlib>
+#include <vector>
+
 using namespace std;
 
-int embaralhar( int vet[], int tam) {
-
-int i, aux;
-
-    for (i = 0; i < 3; i++) {
-        aux = vet[ i ];
-        vet[ i ] = vet[5 - i];
-        vet[ 5 - i ] = aux;
+void embaralhar(vector<int>&v) {
+    for (int i = 0; i < v.size(); i++) {
+        int j = rand() % v.size();
+        swap(v[i], v[j]);
     }
-        for (i = 0; i < tam; i++) {
-            cout << vet[ i ] << "  ";
-        }
 }
   
 int main() 
 {
-
-int vetX[ 6 ], i;
-
-    for (i = 0; i < 6; i++) {
+    vector<int> v(10);
+    for (int i = 0; i < v.size(); i++) {
         cout << "Digite um valor: " << endl;
-        cin >> vetX[ i ];
+        cin >> v[i];
     }
-
-    embaralhar(vetX, 6);
-
-return 0;
+    
+    embaralhar(v);
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
