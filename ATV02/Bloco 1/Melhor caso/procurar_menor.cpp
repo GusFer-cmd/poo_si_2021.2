280d39abd1ac0ae18fe4e262fa4e77b4e519119b
Gustavo Fernandes <g.gustavo5556@gmail.com>
2022-02-01 09:23:18 -0300

    Listas dos estressados

--- ATV02/Bloco 1/Melhor caso/procurar_menor.cpp
+++ ATV02/Bloco 1/Melhor caso/procurar_menor.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 int procurar_menor ( int vet[], int tam) {
--- ATV02/Bloco 1/Melhor caso/procurar_menor_pos.cpp
+++ ATV02/Bloco 1/Melhor caso/procurar_menor_pos.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std; 
 
 int procurar_menor_pos ( int vet[], int tam ) {
