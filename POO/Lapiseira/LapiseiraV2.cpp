f33f104cc926774ea2880162a9569342fc837ca9
Gustavo Fernandes <g.gustavo5556@gmail.com>
2022-02-01 21:12:52 -0300

    POO/Arcade

--- POO/Lapiseira/LapiseiraV2.cpp
+++ POO/Lapiseira/LapiseiraV2.cpp
@@ -6,16 +6,20 @@
 using namespace std;
 
 class Grafite {
+    
+    //Declarando variáveis
     public:
     float calibre;
     int tamanho;
     string dureza;
 
+    //Construtor
     Grafite(float calibre, int tamanho, string dureza) : 
         calibre{calibre}, tamanho{tamanho}, dureza{dureza} {
     }
-       
-    friend ostream& operator<<(ostream& os, const Grafite& grafite) {
+
+    //Friend (Operador de saída).
+    friend ostream& operator<<(ostream& os, const Grafite& grafite) { //Constante posso usar "&"
         os << "Calibre: " << grafite.calibre << "mm, ";
         os << "Tamanho: " << grafite.tamanho << "mm, ";
         os << "Dureza: " << grafite.dureza << ".";
@@ -23,68 +27,69 @@ class Grafite {
     }
 };
 
+//Nova classe
 class Lapiseira {
+    //shared ptr 
     vector<shared_ptr<Grafite>> bico;
-    list<shared_ptr<Grafite>> tambor; 
+    list<shared_ptr<Grafite>> tambor; //Elemento independente
 
 public:
-    Lapiseira( int qtdGrafites) : bico(qtdGrafites, nullptr){
+    Lapiseira( int qtdGrafites) : bico(qtdGrafites, nullptr){ //Construtor da lapiseira
     }
 
     void adicionaGrafite(shared_ptr<Grafite>& grafite) {
-        this->tambor.push_back(grafite);
+        this->tambor.push_back(grafite); //Colocar grafite no final do tambor.
     }
 
-    bool colocarGrafite(int indice) {
-        if (indice < 0 || indice >= this->bico.size()) {
+    bool colocarGrafite(int indice) { //Tambor não existe
+        if (indice < 0 || indice >= this->bico.size()) { 
             cout << "Grafite inexistente\n";
             return false;
         }
-        if (this->bico[indice] != nullptr) {
+        if (this->bico[indice] != nullptr) { //Tambor já tem grafite
             cout << "Grafite ja esta sendo usado\n";
             return false;
         }
-        if (this->tambor.empty()) {
+        if (this->tambor.empty()) { //Não tem grafite.
             cout << "Nao ha grafites no tambor\n";
             return false;
         }
-        this->bico[indice] = this->tambor.front();
-        this->tambor.pop_front();
+        this->bico[indice] = this->tambor.front(); //Ponta do tambor para o bico. Lista não usa índice
+        this->tambor.pop_front(); //Tira o primeiro
         return true;
     }    
 
     bool removerGrafite(int indice) {
-        if (indice < 0 || indice >= this->bico.size()) {
+        if (indice < 0 || indice >= this->bico.size()) { //Existe ou não
             cout << "Grafite inexistente\n";
             return false;
         }
-        if (this->bico[indice] == nullptr) {
+        if (this->bico[indice] == nullptr) { //Sem grafite
             cout << "Bico sem grafite\n";
             return false;
         }
-        this->bico[indice] = nullptr; // remove grafite do bico e ele é descartado
+        this->bico[indice] = nullptr; //Remove grafite do bico e ele é descartado
         return true;
     }
 
-    friend ostream& operator<<(ostream& os, const Lapiseira& lapiseira) {
-        os << "Grafite no bico: | ";
+    friend ostream& operator<<(ostream& os, const Lapiseira& lapiseira) { //Friend
+        os << "Grafite no bico: | "; //Mostrar grafite
         for (int i = 0; i < (int) lapiseira.bico.size(); i++) {
             auto& bico = lapiseira.bico[i];
             os << i << ":";
-            if (bico != nullptr) {
+            if (bico != nullptr) { //Se existir mostra o grafite
                 os << *bico;
             } else {
-                os << "-----";
+                os << "-----"; //Caso não existir
             os << " | ";
             }
         }
-        os << "\nGrafite no tambor: | ";
+        os << "\nGrafite no tambor: | "; //Grafites
         for (auto tambor : lapiseira.tambor) {
             os << *tambor << " ";
         }
         return os;
     }
-
 };
 
 int main () {
@@ -92,7 +97,7 @@ int main () {
     Lapiseira lapiseira(3);
     auto grafite1 = make_shared<Grafite>(1.5, 10, "Duro");
     auto grafite2 = make_shared<Grafite>(2.0, 5, "Duro");
-    auto grafite3 = make_shared<Grafite>(1.5, 10, "Duro");
+    auto grafite3 = make_shared<Grafite>(4.0, 7, "Duro");
 
     lapiseira.adicionaGrafite(grafite1);
     lapiseira.adicionaGrafite(grafite2);
