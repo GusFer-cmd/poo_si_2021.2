6e79ef9c4f6a49b08f5abda28625deb223bf4824
Gustavo Fernandes <g.gustavo5556@gmail.com>
2022-02-05 13:47:27 -0300

    POO/Arcade

--- POO/Cadastro/Cadastro.cpp
+++ POO/Cadastro/Cadastro.cpp
@@ -6,64 +6,64 @@
 using namespace std;
 
 class Conta {
-    protected:
-        int id;
-        float balance{0};
-        string clientId;
-        string type; //CC ou CP
+protected:
+    int id;
+    float balance{0};
+    string clientId;
+    string type; //CC ou CP
 
-    public:
-        Conta(int id, string clientId = "") :id(id), clientId(clientId) {
+public:
+    Conta(int id, string clientId = "") :id(id), clientId(clientId) {
+    }
+
+    virtual void monthupdate() = 0;
+
+    virtual void saque(float valor){
+        if(valor <= 0){
+            cout << "Valor invalido" << endl;
         }
 
-        virtual void monthupdate() = 0;
+        if(valor > this->balance){
+            cout << "Saldo insuficiente" << endl;
+        }            
 
-        virtual void saque(float valor){
-            if(valor <= 0){
-                cout << "Valor invalido" << endl;
-            }
+        cout << "Saque de R$ " << valor << " realizado com sucesso!" << endl;
+        this->balance -= valor;
+     }
 
-            if(valor > this->balance){
-                cout << "Saldo insuficiente" << endl;
-            }            
-
-            cout << "Saque de R$ " << valor << " realizado com sucesso!" << endl;
-            this->balance -= valor;
+    virtual void deposito(float valor){
+        if(valor <= 0){
+            cout << "Valor invalido" << endl;
         }
-
-        virtual void deposito(float valor){
-            if(valor <= 0){
-                cout << "Valor invalido" << endl;
-            }
     
-            cout << "Deposito de R$ " << valor << " realizado com sucesso!" << endl;
-            this->balance += valor;
-        }
+        cout << "Deposito de R$ " << valor << " realizado com sucesso!" << endl;
+        this->balance += valor;
+     }
 
-        virtual void transferir(shared_ptr<Conta> other ,float valor){
-            this->saque(valor);
-            other->deposito(valor);
-        }
+    virtual void transferir(shared_ptr<Conta> other ,float valor){
+        this->saque(valor);
+        other->deposito(valor);
+    }
 
-        virtual int getId() {
-            return this->id;
-        }
+    virtual int getId() {
+        return this->id;
+    }
 
-        virtual float getBalance() {
-            return this->balance;
-        }
+    virtual float getBalance() {
+        return this->balance;
+    }
 
-        virtual string getClientId() {
-            return this->clientId;
-        }
+    virtual string getClientId() {
+        return this->clientId;
+    }
 
-        virtual string getType() {
-            return this->type;
-        }
+    virtual string getType() {
+        return this->type;
+    }
 
-        friend ostream& operator<<(ostream& os, Conta& conta) {
-            os << conta.getId() << ": " << conta.getClientId() << ": " << conta.getBalance() << ": " << conta.getType() << endl;
-        }
+    friend ostream& operator<<(ostream& os, Conta& conta) {
+        os << conta.getId() << ": " << conta.getClientId() << ": " << conta.getBalance() << ": " << conta.getType() << endl;
+    }
 };
 
 class ChecandoConta : public Conta {
@@ -78,7 +78,7 @@ class ChecandoConta : public Conta {
 };
 
 class SalvandoConta : public Conta {
-    public:
+public:
     SalvandoConta(int id, string clientId) : Conta{id, clientId} {
     }
     
@@ -88,116 +88,116 @@ class SalvandoConta : public Conta {
 };
 
 class Cliente {
-    private:
-        string clientId;
-        vector<shared_ptr<Conta>> contas;
+private:
+    string clientId;
+    vector<shared_ptr<Conta>> contas;
 
-    public:
-        Cliente(string clientId = "") : clientId{clientId} {
-        }
+public:
+    Cliente(string clientId = "") : clientId{clientId} {
+    }
         
-        virtual void addConta(const shared_ptr<Conta> &conta){
-            this->contas.push_back(conta);
-        }
+    virtual void addConta(const shared_ptr<Conta> &conta){
+        this->contas.push_back(conta);
+    }
 
-        virtual string getClientId() {
-            return this->clientId;
-        }
+    virtual string getClientId() {
+        return this->clientId;
+    }
 
-        virtual vector<shared_ptr<Conta>> getContas() {
-            return this->contas;
-        }
+    virtual vector<shared_ptr<Conta>> getContas() {
+        return this->contas;
+    }
 
-        virtual void setClientId(string newClientId) {
-            this->clientId = newClientId;
-        }
+    virtual void setClientId(string newClientId) {
+        this->clientId = newClientId;
+    }
 
-        virtual void setContas(vector<shared_ptr<Conta>> contas) {
-            this->contas = contas;
-        }
+    virtual void setContas(vector<shared_ptr<Conta>> contas) {
+        this->contas = contas;
+    }
 
-        friend ostream& operator<<(ostream& os, Cliente& cliente) {
-            os << cliente.clientId << " [ " << endl;
-            for (int i = 0; i < (int)cliente.contas.size(); i++){
-                os << cliente.contas[i]->getClientId() << ((i+1 < (int)cliente.contas.size()) ? "," : "");
-            }
-            os << " ]" << endl;
-            return os;
+    friend ostream& operator<<(ostream& os, Cliente& cliente) {
+        os << cliente.clientId << " [ " << endl;
+        for (int i = 0; i < (int)cliente.contas.size(); i++){
+            os << cliente.contas[i]->getClientId() << ((i+1 < (int)cliente.contas.size()) ? "," : "");
         }
+        os << " ]" << endl;
+        return os;
+    }
 };
 
 class Agencia {
-    private:
-        map<string, shared_ptr<Cliente>> clientes;
-        map<int, shared_ptr<Conta>> contas;
-        int nexContaId{0};
+private:
+    map<string, shared_ptr<Cliente>> clientes;
+    map<int, shared_ptr<Conta>> contas;
+    int nexContaId{0};
 
-        virtual shared_ptr<Conta> getConta(int id){
-            auto it = this->contas.find(id);
-            if(it == this->contas.end()){
-                cout << "Conta nao registrada" << endl;
-            }
-            return it->second;
+    virtual shared_ptr<Conta> getConta(int id){
+        auto it = this->contas.find(id);
+        if(it == this->contas.end()){
+            cout << "Conta nao registrada" << endl;
         }
+        return it->second;
+    }
 
-    public:
-        Agencia(){};
+public:
+    Agencia(){};
 
-        virtual void addConta(string clientId){
-            auto it = this->clientes.find(clientId);
-            if(it != this->clientes.end()){
-                cout << "Cliente registrado" << endl;
-            }
+    virtual void addConta(string clientId){
+        auto it = this->clientes.find(clientId);
+        if(it != this->clientes.end()){
+            cout << "Cliente registrado" << endl;
+        }
         
-            Cliente c{clientId};
+        Cliente c{clientId};
 
-            SalvandoConta sa{this->nexContaId, clientId};
-            c.addConta(make_shared<SalvandoConta>(sa));
-            this->contas[this->nexContaId] = make_shared<SalvandoConta>(sa);
+        SalvandoConta sa{this->nexContaId, clientId};
+        c.addConta(make_shared<SalvandoConta>(sa));
+        this->contas[this->nexContaId] = make_shared<SalvandoConta>(sa);
 
-            this->nexContaId++;
+        this->nexContaId++;
 
-            ChecandoConta cc{this->nexContaId, clientId};
-            c.addConta(make_shared<ChecandoConta>(cc));
-            this->contas[this->nexContaId] = make_shared<ChecandoConta>(cc);
+        ChecandoConta cc{this->nexContaId, clientId};
+        c.addConta(make_shared<ChecandoConta>(cc));
+        this->contas[this->nexContaId] = make_shared<ChecandoConta>(cc);
 
-            this->clientes[clientId] = make_shared<Cliente>(c);
-            this->nexContaId++;
+        this->clientes[clientId] = make_shared<Cliente>(c);
+        this->nexContaId++;
+    }
+
+    virtual void saque(int id, float valor){
+        shared_ptr<Conta> conta{getConta(id)};
+        conta->saque(valor);
+    }
+
+    virtual void deposito(int id, float valor){
+        shared_ptr<Conta> conta{getConta(id)};
+        conta->deposito(valor);
+    }
+
+    virtual void transferir(int id, int id2, float valor){
+        shared_ptr<Conta> conta{getConta(id)};
+        shared_ptr<Conta> conta2{getConta(id2)};
+        conta->transferir(conta2, valor);
+    }
+
+    virtual void monthupdate(){
+        for(auto it = this->contas.begin(); it != this->contas.end(); it++){
+            it->second->monthupdate();
         }
+    }
 
-        virtual void saque(int id, float valor){
-            shared_ptr<Conta> conta{getConta(id)};
-            conta->saque(valor);
+    friend ostream &operator<<(ostream &os, Agencia &agencia) {
+        os << "Clientes: " << endl;
+        for(auto it = agencia.clientes.begin(); it != agencia.clientes.end(); it++){
+            os << *it->second;
         }
-
-        virtual void deposito(int id, float valor){
-            shared_ptr<Conta> conta{getConta(id)};
-            conta->deposito(valor);
-        }
-
-        virtual void transferir(int id, int id2, float valor){
-            shared_ptr<Conta> conta{getConta(id)};
-            shared_ptr<Conta> conta2{getConta(id2)};
-            conta->transferir(conta2, valor);
-        }
-
-        virtual void monthupdate(){
-            for(auto it = this->contas.begin(); it != this->contas.end(); it++){
-                it->second->monthupdate();
-            }
-        }
-
-        friend ostream &operator<<(ostream &os, Agencia &agencia) {
-            os << "Clientes: " << endl;
-            for(auto it = agencia.clientes.begin(); it != agencia.clientes.end(); it++){
-                os << *it->second;
-            }
             os << "Contas: " << endl;
             for(auto it = agencia.contas.begin(); it != agencia.contas.end(); it++){
                 os << *it->second;
             }
             return os;
-        }
+    }
 };
 
 int main() {
