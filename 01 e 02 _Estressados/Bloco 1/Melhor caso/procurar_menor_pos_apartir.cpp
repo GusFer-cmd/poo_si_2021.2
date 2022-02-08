7ef18be3c64d8bbfaa6a004476b8565bcfa30427
Gustavo Fernandes <g.gustavo5556@gmail.com>
2022-02-08 20:01:14 -0300

    Listas dos estressados

--- 01 e 02 _Estressados/Bloco 1/Melhor caso/procurar_menor_pos_apartir.cpp
+++ 01 e 02 _Estressados/Bloco 1/Melhor caso/procurar_menor_pos_apartir.cpp
@@ -3,26 +3,16 @@
 
 using namespace std;
 
-int procurar_menor_pos_apartir(vector<int> v){ //Função que recebe como parametro um vector
-
-int i, num, PMPA, encontrado = 0, contador = 0; //Declaração de variáveis
-
-    cout << "Digite um numero que voce deseja procurar: " << endl;
-    cin >> num;
-
-        for (i = 0; i < v.size(); i++) { //Laço
-            if ( v[ i ] == num) { //Condição que verifica se o valor da posição do vetor é igual ao numero digitado pelo usuario
-                encontrado = 1; //Se o valor da posição do vetor for igual ao numero digitado pelo usuario, o valor de encontrado recebe 1
-                PMPA = i + 2;
-                contador++; //Contador que conta quantas vezes o numero digitado aparece no vetor
-            } 
+int procurar_menor_pos_apartir(vector<int> v, int pos){ //Função que recebe como parametro um vector
+int i, posM = 0, menor = v[ 0 ]; //Declaração de variáveis
+    
+    for ( i = pos + 1; i < v.size(); i++) { //Laço
+        if ( v[ i ] < menor){ //Condição
+            menor = v[ i ]; //Atribui o menor valor ao elemento i do vetor
+            posM = i + 1; //Posição
         }
+    }
 
-        if(encontrado == 0){ 
-            cout << "Numero nao existe: " << endl;
-        } else {
-            cout << "Numero " << num << " aparece " << contador << " x no vetor." << " E sua proxima posicao: " << PMPA << endl;
-        }
 }
 
 int main(){
@@ -33,7 +23,7 @@ int main(){
         cin >> v[ i ]; //Recebe o valor digitado
     }
 
-    procurar_menor_pos_apartir(v); //Chamada da função
+    procurar_menor_pos_apartir(v, 2); //Chamada da função
 
     return 0;
 }
